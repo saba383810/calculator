a13687ec003066d03b590f2aa2b99102420497cd
saba383810 <saba383810@gmail.com>
2021-01-14 12:02:00 +0900

    電卓のデザイン変更&パーセント計算、ドット計算を追加

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,7 @@ public class Controller {
 
     public static String formula = "";
     public int strCnt=0;
+    public Button b;
 
     ScriptEngineManager manager = new ScriptEngineManager();
     ScriptEngine engine = manager.getEngineByName("js");
@@ -42,31 +43,61 @@ public class Controller {
 
     @FXML
     void Equal(ActionEvent event) throws ScriptException {
-        if(!(formula.endsWith("+")||formula.endsWith("-")||formula.endsWith("×")||formula.endsWith("÷"))) {
+        if(!(formula.endsWith("+")||formula.endsWith("-")||formula.endsWith("×")||formula.endsWith("÷")||formula.endsWith("."))) {
 
             formula = formula.replace("×", "*");
             formula = formula.replace("÷", "/");
             formula = engine.eval(formula).toString();
+            strCnt = formula.length();
             textField.setText(formula);
+
         }
     }
 
     @FXML
     void Op(ActionEvent event) {
-        Button b = (Button)event.getSource();
-        if(formula.endsWith("+")||formula.endsWith("-")||formula.endsWith("×")||formula.endsWith("÷"))
+        b = (Button)event.getSource();
+        if(formula.endsWith("+")||formula.endsWith("-")||formula.endsWith("×")||formula.endsWith("÷")) {
             formula = formula.substring(0, formula.length() - 1) + b.getText();
-        else if(!(formula.length()==0))
-            formula+=b.getText();
-
-        textField.setText(formula);
+        }else if(!(formula.length()==0)) {
+            formula += b.getText();
+        }
         strCnt=0;
+        textField.setText(formula);
     }
 
     @FXML
     void addFormula(KeyEvent event) {
         formula=textField.getText();
-        System.out.println(formula);
+    }
+
+    @FXML
+    void delete(ActionEvent event){
+        if(formula.endsWith("+")||formula.endsWith("-")||formula.endsWith("×")||formula.endsWith("÷"));
+        else if(!(formula.length()==0)&&strCnt!=0) {
+            formula = formula.substring(0, formula.length() - 1);
+            strCnt--;
+        }
+        textField.setText(formula);
+    }
+    @FXML
+    void Percent(ActionEvent event) {
+        if(!(formula.endsWith("+")||formula.endsWith("-")||formula.endsWith("×")||formula.endsWith("÷")||formula.endsWith("."))&&formula.length()!=0&&strCnt!=0){
+            String str=formula.substring(formula.length()-strCnt, formula.length());
+            float num =Float.parseFloat(str);
+            float ans = num/100;
+            formula = formula.substring(0, formula.length() - strCnt)+ans;
+            textField.setText(formula);
+        }
+    }
+    @FXML
+    void Dot(ActionEvent event){
+        b = (Button)event.getSource();
+        if(!(formula.endsWith("+")||formula.endsWith("-")||formula.endsWith("×")||formula.endsWith("÷"))&&!(formula.length()==0)&&!(formula.endsWith("."))) {
+            formula += b.getText();
+            strCnt++;
+        }
+        textField.setText(formula);
     }
 
 }
